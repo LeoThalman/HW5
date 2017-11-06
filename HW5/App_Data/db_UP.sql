f8811df813b0cb5633feac2a99fb86dc28db9a37
LeoThalman <lthalman15@wou.edu>
2017-11-05 17:20:13 -0800

    Added error checking to database

--- HW5/App_Data/db_UP.sql
+++ HW5/App_Data/db_UP.sql
@@ -13,7 +13,7 @@ CREATE TABLE dbo.DMVs
 );
 
 INSERT INTO dbo.DMVs (Permit,FullName,DOB,ResidenceAddress,City,StateAbbreviated,ZipCode,County) VALUES
-			(8673443, 'Frank Johnson','1942-03-13 00:00:00', '1432 Wyrmwood Rd.' , 'Dallas', 'OR', 33442, 'Adelaide'),
+			(8673443, 'Frank Johnson','1942-03-13 00:00:00', '1432 Wyrmwood Rd.' , 'Dallas', 'OR', 33442, 'Polk'),
 			(1937542, 'Frannie Kay','1978-12-1 00:00:00', '132 1st St.' , 'Olympia', 'WA', 42622, 'Filstin'),
 			(8673443, 'Julie Mac','1993-04-25 00:00:00', '6934 Brunst Rd.' , 'Debran', 'GA', 33442, 'Colston'),
 			(8673443, 'Mikhail Monroe','1978-08-09 00:00:00', '192 Central Ave.' , 'Stockland', 'OK', 19276, 'Cricklin'),
